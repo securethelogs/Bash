9586a08819614ffea4f2bd777a1bbd962e90b3c8
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-06-05 15:28:21 +0100

    Update rainfall.sh

--- rainfall.sh
+++ rainfall.sh
@@ -12,7 +12,7 @@ echo "Scanning..."
 if [ -z "$NM" ]
 then
 
-        for port in {21,22,80,139,443,445,1433,3389,5895,8080}; do
+        for PORT in {21,22,80,139,443,445,1433,3389,5895,8080}; do
                 (timeout 0.0 echo >/dev/tcp/$IP/$PORT) > /dev/null 2>&1 &&
                         echo "Port $PORT is open" ||
                         echo "Port $PORT is closed"
